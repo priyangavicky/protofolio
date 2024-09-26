62b3221ba6d0d0cd46898dba69648fe17e348e7f
Priyanga <110669229+priyangavicky@users.noreply.github.com>
2024-09-26 11:09:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,34 +10,37 @@ import Websites from './components/Websites';
 import Projects from './components/Projects';
 import Mailoverview from './components/bulkmailoverview';
 import Todo from './components/todooverview';
+// Import NotFound for 404 handling if you create it
+// import NotFound from './components/NotFound'; 
 
-
-
+const Home = () => (
+  <>
+    <Header />
+    <Hero />
+    <About />
+    <Websites />
+    <Projects />
+    <Contact />
+    <Footer />
+  </>
+);
 
 function App() {
-  const basename = process.env.NODE_ENV === 'production' ? '/' : '/';
+  /* const basename = process.env.NODE_ENV === 'production' ? '/subdirectory' : '/'; */
 
   return (
-   <>
     <div className="App">
-      <Router basename={basename}>
-      <Routes>
-      <Route path="/" element={<React.Fragment>
-        <Header/>
-        <Hero />
-        <About/>
-        <Websites/>
-        <Projects/>
-        <Contact/>
-        <Footer/>
-       </React.Fragment>} />
-       <Route path="/bulkmailoverview" element={<Mailoverview />} />
-       <Route path="/todooverview" element={<Todo />} />
-       </Routes>
-</Router>
+      <Router basename='/'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bulkmailoverview" element={<Mailoverview />} />
+          <Route path="/todooverview" element={<Todo />} />
+          {/* 404 Route */}
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </Router>
     </div>
-   </>
   );
 }
 
-export default App;
+export default App
